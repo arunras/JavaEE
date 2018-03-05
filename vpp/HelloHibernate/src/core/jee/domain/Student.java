5e99d623edeae8a7032637a354033df659daf2b0
Arun <prith000@citymail.cuny.edu>
2018-03-05 09:57:34 -0500

    4. Configuring Hibernate

--- vpp/HelloHibernate/src/core/jee/domain/Student.java
+++ vpp/HelloHibernate/src/core/jee/domain/Student.java
@@ -6,52 +6,47 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
- * Represents a Student enrolled in the college management
- * system (CMS)
+ * Represents a Student enrolled in the college management system (CMS)
  */
 
 @Entity
-public class Student
-{
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-    private String enrollmentID;
-    private String name;
-    private String tutorName; // This will become a class soon
-    
-    
-    public Student() {/*Required by JPA*/}
-    
-    
-    /**
-     * Initialises a student with a particular tutor
-     */
-    public Student(String name, String tutorName)
-    {
-    	this.name = name;
-    	this.tutorName = tutorName;
-    }
-    
-    /**
-     * Initialises a student with no pre set tutor
-     */
-    public Student(String name)
-    {
-    	this.name = name;
-    	this.tutorName = null;
-    }
-    
-    public double calculateGradePointAverage()
-    {
-    	// some complex business logic!
-    	// we won't need this method on the course, BUT it is import
-    	// to remember that classes aren't just get/set pairs - we expect
-    	// business logic in here as well.
-    	return 0;
-    }
-    
-    @Override
-    public String toString() {
-    		return this.name;
-    }
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String enrollmentID;
+	private String name;
+	private String tutorName; // This will become a class soon
+
+	public Student() {
+		/* Required by JPA */}
+
+	/**
+	 * Initialises a student with a particular tutor
+	 */
+	public Student(String name, String tutorName) {
+		this.name = name;
+		this.tutorName = tutorName;
+	}
+
+	/**
+	 * Initialises a student with no pre set tutor
+	 */
+	public Student(String name) {
+		this.name = name;
+		this.tutorName = null;
+	}
+
+	public double calculateGradePointAverage() {
+		// some complex business logic!
+		// we won't need this method on the course, BUT it is import
+		// to remember that classes aren't just get/set pairs - we expect
+		// business logic in here as well.
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
 }
